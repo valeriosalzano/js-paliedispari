41acf25020e82959ce071d4236488e37e175502b
Valerio Salzano <vlr.slzn@gmail.com>
2023-03-02 16:49:08 +0100

    pari e dispari conclusion

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@
 // Chiedere all’utente di inserire una parola
 const userPalindromeDom = document.getElementById("userPalindrome");
 
-
-
 // richiamo la funzione al click del pulsante
 const palindromeCheckBtn = document.getElementById('palindromeCheckBtn');
 palindromeCheckBtn.addEventListener('click', 
@@ -13,16 +11,18 @@ palindromeCheckBtn.addEventListener('click',
         const userPalindrome = userPalindromeDom.value;
         // verifico se il valore è corretto
         if (userPalindrome.length < 2 || userPalindrome.trim() === '') {
+            // valore non corretto
             alert('Inserisci un valore valido')
         } else {
+            // verifica superata, invoco la funzione
             let isPalindrome = palindromeCheck(userPalindrome);
+            // controllo l'esito fornito dalla funzione
             if (isPalindrome){
                 alert(` Esatto! "${userPalindrome}" è un palindromo.`);
             } else {
                 alert(` Peccato, "${userPalindrome}" non è un palindromo.`);
             }
-        }
-            
+        }    
     }
 );
 
@@ -33,7 +33,6 @@ palindromeResetBtn.addEventListener('click',
         userPalindromeDom.value = '';
     }
 ) 
-
 
 // Creare una funzione per capire se la parola inserita è palindroma
 function palindromeCheck (userInput){
@@ -57,8 +56,57 @@ function palindromeCheck (userInput){
 
 
 // Pari e Dispari
+
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+const evenBetBtn = document.getElementById('evenBetBtn');
+const userNumberDom = document.getElementById('userNumber');
+
+evenBetBtn.addEventListener('click',
+    function(){
+        let userBet = 'even';
+        let userNumber = parseInt(userNumberDom.value);
+        gameRound(userNumber,userBet);
+    }
+)
+
+const oddBetBtn = document.getElementById('oddBetBtn');
+oddBetBtn.addEventListener('click',
+    function(){
+        let userBet = 'odd';
+        let userNumber = parseInt(userNumberDom.value);
+        gameRound(userNumber,userBet);
+    }
+)
+
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+function pcNumber (){
+    return Math.floor(Math.random()*5)+1;
+}
 // Sommiamo i due numeri
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+function EvenOrOddSum (a, b){
+    alert(`Hai scelto ${a}. Il computer ha scelto ${b}.`)
+    let sum = a + b;
+    if (sum % 2 == 0){
+        return 'even'
+    } else {
+        return 'odd'
+    }
+}
 // Dichiariamo chi ha vinto.
+function gameRound (userNumber, userBet ){
+    if (userNumber > 0 && userNumber < 6 ){
+        // numero valido
+        let winningBet = EvenOrOddSum(userNumber,pcNumber());
+        if(userBet == winningBet){
+            // vittoria dell'utente 
+            alert(`Complimenti! Hai vinto!`);
+        } else {
+            // vittoria del pc 
+            alert(`Peccato! Ritenta.`);
+        }
+    } else {
+        // numero non valido
+        alert('Scrivi un numero valido.');
+    }
+}
